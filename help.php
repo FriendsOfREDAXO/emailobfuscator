3ea7d31f52bc3ac38ac3395cdfb1f4c469cb8623
Thomas Kaegi <tk@meeloo.ch>
2016-03-05 11:03:45 +0100

    Fixes #3

--- help.php
+++ help.php
@@ -1,28 +1,37 @@
-<p>Dieses kleine aber praktische AddOn "verschüsselt" Emailadressen (als Links oder Plain) im Quelltext und "entschlüsselt" sie wieder via Javascript.</p>
-
-<p>Benötigt wird dafür ein CSS Snippet zur Darstellung und eine Javascriptfunktion zur entschlüsselung. Beide Files werden mit diesem AddOn mitgeliefert und sollten wie folgt eingebunden werden:</p>
 <?php
-	$css = "";
-	$css .= "<?php".PHP_EOL;
-	$css .= "  if (rex_addon::get('rex_emailobfuscator')->isAvailable()) {".PHP_EOL;
-	$css .= "    ?>".PHP_EOL;
-	$css .= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"<?=rex_url::addonAssets('rex_emailobfuscator', 'rex_emailobfuscator.css');?>\">".PHP_EOL;
-	$css .= "    <?php".PHP_EOL;
-	$css .= "  }".PHP_EOL;
-	$css .= "?>";
+	echo rex_view::info($this->i18n('help_infotext1').'<br><br>'.$this->i18n('help_infotext2'));
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
+	$code .= "  if (rex_addon::get('rex_emailobfuscator')->isAvailable()) {".PHP_EOL;
+	$code .= "    ?>".PHP_EOL;
+	$code .= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"<?=rex_url::addonAssets('rex_emailobfuscator', 'rex_emailobfuscator.css');?>\">".PHP_EOL;
+	$code .= "    <?php".PHP_EOL;
+	$code .= "  }".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', 'CSS File einbinden', false); //todo
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
+	$code .= "  if (rex_addon::get('rex_emailobfuscator')->isAvailable()) {".PHP_EOL;
+	$code .= "    ?>".PHP_EOL;
+	$code .= "    <script src=\"<?=rex_url::addonAssets('rex_emailobfuscator', 'rex_emailobfuscator.js');?>\"></script>".PHP_EOL;
+	$code .= "    <?php".PHP_EOL;
+	$code .= "  }".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', 'JS File einbinden', false); //todo
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	echo rex_view::info($this->i18n('help_infotext3'));
 ?>
-<?=rex_string::highlight($css);?>
-
-<?php
-	$js = "";
-	$js .= "<?php".PHP_EOL;
-	$js .= "  if (rex_addon::get('rex_emailobfuscator')->isAvailable()) {".PHP_EOL;
-	$js .= "    ?>".PHP_EOL;
-	$js .= "    <script src=\"<?=rex_url::addonAssets('rex_emailobfuscator', 'rex_emailobfuscator.js');?>\"></script>".PHP_EOL;
-	$js .= "    <?php".PHP_EOL;
-	$js .= "  }".PHP_EOL;
-	$js .= "?>";
-?>
-<?=rex_string::highlight($js);?>
-
-<p>Grundsätzlich kann man die Inhalte der beiden Files auch in eigene Files kopieren, jedoch müsste man dann die Files im Falle eines AddOn-Updates manuell anpassen.</p>
