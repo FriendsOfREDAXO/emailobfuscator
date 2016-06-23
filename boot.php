8f3c8e2ab24068d4d5b3e3c0e9e776abd26a67eb
Thomas Kaegi <tk@meeloo.ch>
2016-06-23 08:48:46 +0200

    Verschlüsselung verhindern für Artikel/Templates hinzugefügt

--- boot.php
+++ boot.php
@@ -1,11 +1,17 @@
 <?php
 	if (!rex::isBackend()) {
+		
 		rex_extension::register('OUTPUT_FILTER',function(rex_extension_point $ep) {
-			$subject = $ep->getSubject();
-			$subject = preg_replace_callback('/(\'|\"|)(mailto:[A-Za-z0-9]([A-Za-z0-9._+-]*[A-Za-z0-9]|())@[A-Za-z0-9]([A-Za-z0-9.\-]*[A-Za-z0-9]|())\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmailLinks', $subject);
-			$subject = preg_replace_callback('/([A-Za-z0-9](?:[A-Za-z0-9._+-]*[A-Za-z0-9]|(?:)))@([A-Za-z0-9](?:[A-Za-z0-9.\-]*[A-Za-z0-9]|(?:))\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmail', $subject);
+			$whitelistArticles = rex_addon::get('rex_emailobfuscator')->getConfig('articles');
+			$whitelistTemplates = rex_addon::get('rex_emailobfuscator')->getConfig('templates');
 			
-			$ep->setSubject($subject);
+			if (!in_array(rex_article::getCurrentId(),$whitelistArticles) && !in_array(rex_article::getCurrent()->getTemplateId(), $whitelistTemplates)) {
+				$subject = $ep->getSubject();
+				$subject = preg_replace_callback('/(\'|\"|)(mailto:[A-Za-z0-9]([A-Za-z0-9._+-]*[A-Za-z0-9]|())@[A-Za-z0-9]([A-Za-z0-9.\-]*[A-Za-z0-9]|())\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmailLinks', $subject);
+				$subject = preg_replace_callback('/([A-Za-z0-9](?:[A-Za-z0-9._+-]*[A-Za-z0-9]|(?:)))@([A-Za-z0-9](?:[A-Za-z0-9.\-]*[A-Za-z0-9]|(?:))\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmail', $subject);
+				
+				$ep->setSubject($subject);
+			}
 		});
 	}
 ?>
