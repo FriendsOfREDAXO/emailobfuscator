fcc2882566b02cd3feef4219537a40a4d65c8ab1
Thomas Kaegi <tk@meeloo.ch>
2016-07-15 14:42:20 +0200

    Parsing angepasst

--- boot.php
+++ boot.php
@@ -2,10 +2,15 @@
 	if (!rex::isBackend()) {
 		
 		rex_extension::register('OUTPUT_FILTER',function(rex_extension_point $ep) {
-			$whitelistArticles = rex_addon::get('rex_emailobfuscator')->getConfig('articles', []);
 			$whitelistTemplates = rex_addon::get('rex_emailobfuscator')->getConfig('templates', []);
+			$whitelistArticles = rex_addon::get('rex_emailobfuscator')->getConfig('articles', []);
+			if (!empty($whitelistArticles)) {
+				$whitelistArticles = explode(',', $whitelistArticles);
+			}
 			
-			if (!in_array(rex_article::getCurrentId(),$whitelistArticles) && !in_array(rex_article::getCurrent()->getTemplateId(), $whitelistTemplates)) {
+			
+			
+			if (!in_array(rex_article::getCurrent()->getTemplateId(), $whitelistTemplates) && !in_array(rex_article::getCurrentId(),$whitelistArticles)) {
 				$subject = $ep->getSubject();
 				$subject = preg_replace_callback('/(\'|\"|)(mailto:[A-Za-z0-9]([A-Za-z0-9._+-]*[A-Za-z0-9]|())@[A-Za-z0-9]([A-Za-z0-9.\-]*[A-Za-z0-9]|())\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmailLinks', $subject);
 				$subject = preg_replace_callback('/([A-Za-z0-9](?:[A-Za-z0-9._+-]*[A-Za-z0-9]|(?:)))@([A-Za-z0-9](?:[A-Za-z0-9.\-]*[A-Za-z0-9]|(?:))\.[A-Za-z]+)/', 'rex_emailobfuscator::encodeEmail', $subject);
